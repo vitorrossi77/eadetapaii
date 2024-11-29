e1d8466bef86952641f7810c822f0e58c019811f
vitorrossi77 <vitorrossidoamaral@gmail.com>
2024-11-29 11:01:18 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Exibe uma mensagem ao clicar no botão
+function mostrarMensagem() {
+    alert("Obrigado por visitar meu site! Fique à vontade para explorar.");
+}
+
+// Exemplo de evento ao carregar a página
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Site carregado com sucesso!");
+});
